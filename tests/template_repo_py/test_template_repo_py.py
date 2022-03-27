ee7499cd8fe948da374cdc901736ee743cc8b223
libcommon <57610970+libcommon@users.noreply.github.com>
2022-03-26 22:20:41 -0500

    Add src and tests directories

---
+++ tests/template_repo_py/test_template_repo_py.py
@@ -0,0 +1,7 @@
+def test_template_repo_py_importable() -> None:
+    """Test that the package is importable.
+
+    You can safely remove this unit test once implementing at least one other,
+    though you should always test that the package is importable.
+    """
+    import template_repo_py  # isort:skip pylint: disable=import-outside-toplevel,unused-import
